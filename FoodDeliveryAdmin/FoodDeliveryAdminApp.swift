563f93ec2672ca5a37f761f393e6e6fe7bf51790
PascalAllekotte <pascalallekotte1@web.de>
2024-08-24 15:59:26 +0200

    Initial Commit

---
+++ FoodDeliveryAdmin/FoodDeliveryAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAdminApp.swift
+//  FoodDeliveryAdmin
+//
+//  Created by Pascal Allekotte on 24.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
